1877daecb0f39e1bdaa619c01cc51256b53e03ee
tangweix <weix.tang@intel.com>
2022-02-14 17:57:57 +0800

    Change ro.enable.native.bridge.exec to ro.vendor.enable.native.bridge.exec
     Tracked-On: OAM-100984
     Signed-off-by: tangwei <weix.tang@intel.com>

--- houdini.mk
+++ houdini.mk
@@ -3,9 +3,9 @@ WITH_NATIVE_BRIDGE := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.dalvik.vm.isa.arm=x86 \
-    ro.enable.native.bridge.exec=1 \
+    ro.vendor.enable.native.bridge.exec=1 \
     ro.dalvik.vm.isa.arm64=x86_64 \
-    ro.enable.native.bridge.exec64=1 \
+    ro.vendor.enable.native.bridge.exec64=1 \
     ro.dalvik.vm.native.bridge=libhoudini.so
 
 PRODUCT_PACKAGES += \
