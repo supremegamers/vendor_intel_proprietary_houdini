1ecd060082ab16ec343b2fb07778aacd083fc493
SGNight <thisismycoolid@gmail.com>
2022-04-03 01:06:43 +0530

    Update to latest WSA houdini

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vendor_intel_proprietary_houdini
+# Microsoft WSA Houdini for intel/AMD x86 CPU
+
+Version (x86) = 11.0.1b_y.38765.m
+Version (x86_64) = 11.0.1b_z.38765.m
--- houdini.mk
+++ houdini.mk
@@ -4,7 +4,16 @@ WITH_NATIVE_BRIDGE := true
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.dalvik.vm.isa.arm=x86 \
     ro.enable.native.bridge.exec=1 \
-    ro.dalvik.vm.native.bridge=libhoudini.so
+    ro.dalvik.vm.isa.arm64=x86_64 \
+    ro.enable.native.bridge.exec64=1 \
+    ro.dalvik.vm.native.bridge=libhoudini.so \
+    ro.bionic.arch=x86_64 \
+    ro.bionic.2nd_arch=x86 \
+    persist.sys.dalvik.vm.lib.2=libart.so \
+    dalvik.vm.isa.x86_64.variant=x86_64 \
+    dalvik.vm.isa.x86_64.features=default \
+    dalvik.vm.isa.x86.variant=x86_64 \
+    dalvik.vm.isa.x86.features=default
 
 HOUDINI_PATH := $(dir $(LOCAL_PATH))houdini/prebuilts
 PRODUCT_COPY_FILES += \
