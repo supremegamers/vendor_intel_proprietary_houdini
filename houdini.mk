c29276a5c8e6621c65ff26e2eee1a288db514a46
hmtheboy154 <buingoc67@gmail.com>
2022-08-18 16:12:05 +0700

    Remove props that are not needed

--- houdini.mk
+++ houdini.mk
@@ -6,14 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.enable.native.bridge.exec=1 \
     ro.dalvik.vm.isa.arm64=x86_64 \
     ro.enable.native.bridge.exec64=1 \
-    ro.dalvik.vm.native.bridge=libhoudini.so \
-    ro.bionic.arch=x86_64 \
-    ro.bionic.2nd_arch=x86 \
-    persist.sys.dalvik.vm.lib.2=libart.so \
-    dalvik.vm.isa.x86_64.variant=x86_64 \
-    dalvik.vm.isa.x86_64.features=default \
-    dalvik.vm.isa.x86.variant=x86_64 \
-    dalvik.vm.isa.x86.features=default
+    ro.dalvik.vm.native.bridge=libhoudini.so
 
 HOUDINI_PATH := $(dir $(LOCAL_PATH))houdini/prebuilts
 PRODUCT_COPY_FILES += \
